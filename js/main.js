353d5affe9e1e1752838105edc157e913ab2a336
BenAvrahamos <159269513+BenAvrahamos@users.noreply.github.com>
2024-02-07 17:56:04 +0200

    Minesweeper- basic

--- js/main.js
+++ js/main.js
@@ -2,6 +2,13 @@
 
 var gBoard
 
+var gGame = {
+    isOn: true,
+    shownCount: 0,
+    markedCount: 0,
+    secsPassed: 0
+}
+
 var gLevel = {
     SIZE: 4,
     MINES: 2
@@ -37,15 +44,16 @@ function buildBoard(ROWS, COLS) {
                 isMine: false,
                 isMarked: false
             }
-            if (i === 2 & j === 2) {
-                board[i][j].isMine = true
-            }
-            if (i === 3 & j === 3) {
-                board[i][j].isMine = true
-            }
+            // if (i === 2 & j === 2) {
+            //     board[i][j].isMine = true
+            // }
+            // if (i === 3 & j === 3) {
+            //     board[i][j].isMine = true
+            // }
 
         }
     }
+    randomMines(board)
     setMinesNeighsCount(board)
     console.log(board);
     return board
@@ -106,12 +114,16 @@ function countNeighbors(cellI, cellJ, board,) {
 }
 
 function onCellClicked(cell, i, j) {
+    if (gGame.isOn === false) return
+    gBoard[i][j].isShown = true
     const elCell = cell
+    elCell.classList.add('shown')
     const elCellId = elCell.getAttribute('id')
+
     console.log(elCellId);
-    if (elCellId === '0') {
-        // revealNeighbors(i, j, gBoard,)
-    }
+    // if (elCellId === '0') {
+    //     revealNeighbors(i, j, gBoard,)
+    // }
 
     elCell.classList.remove('hide')
 
@@ -121,25 +133,61 @@ function onCellClicked(cell, i, j) {
 
 }
 
-// function revealNeighbors(cellI, cellJ, board,) {
-//     var neighborsCount = 0
-//     for (var i = cellI - 1; i <= cellI + 1; i++) {
-//         if (i < 0 || i >= board.length) continue //skips tiles beyond border of mat
-//         for (var j = cellJ - 1; j <= cellJ + 1; j++) {
-//             if (i === cellI && j === cellJ) continue//skip self
-//             if (j < 0 || j >= board[i].length) continue //skips tiles beyond border of mat
-//             if (board[i][j].isMine === false){
-//                 const elCell = document.querySelector(`[data-i="${cellI}"][data-j="${cellJ}"]`)
-//                 // elCell.classList.remove('hide')
-//                 console.log(elCell);
-                
 
-//             }
+function randomMines(gBoard) {
+    for (var i = 0; i <= gLevel.MINES - 1; i++) {
+        const randomI = getRandomInt(0, gLevel.SIZE)
+        const randomJ = getRandomInt(0, gLevel.SIZE)
+        while (gBoard[randomI][randomJ].isMine === false) {
+            (gBoard[randomI][randomJ].isMine === false) ? gBoard[randomI][randomJ].isMine = true : i - 1
 
-//         }
-//     }
-//     return neighborsCount
-// }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function revealNeighbors(cellI, cellJ, board,) {
+    for (var i = cellI - 1; i <= cellI + 1; i++) {
+        // if (board[cellI][cellJ].isShown === true)return
+        if (i < 0 || i >= board.length) continue //skips tiles beyond border of mat
+        for (var j = cellJ - 1; j <= cellJ + 1; j++) {
+            const currCell = board[i][j]
+            if (i === cellI && j === cellJ) continue//skip self
+            if (j < 0 || j >= board[i].length) continue //skips tiles beyond border of mat
+            if (currCell.isMine === false) {
+                const elCell = document.querySelector(`[data-i="${i}"][data-j="${j}"]`)
+                currCell.isShown = true
+                console.log(currCell.isShown);
+                elCell.classList.remove('hide')
+                console.log('updated', gBoard);
+            }
+            if (currCell.minesAroundCount === 0) revealNeighbors(i, j, gBoard,)
+
+
+        }
+    }
+
+}
+
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min)
+    max = Math.floor(max)
+    return Math.floor(Math.random() * (max - min)) + min //The maximum is inclusive and the minimum is inclusive
+}
+
 
 
 
