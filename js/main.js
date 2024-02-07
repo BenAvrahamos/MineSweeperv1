a7675cd2aa7df505592dbc07e90e3d3b525c424a
BenAvrahamos <159269513+BenAvrahamos@users.noreply.github.com>
2024-02-07 20:43:49 +0200

    MineSweeper
    Most mandatory tasks complete
    missing features:
    -Flags
    -mines POST first click
    -Style :)

--- js/main.js
+++ js/main.js
@@ -6,7 +6,8 @@ var gGame = {
     isOn: true,
     shownCount: 0,
     markedCount: 0,
-    secsPassed: 0
+    secsPassed: 0,
+    lives: 3
 }
 
 var gLevel = {
@@ -21,7 +22,7 @@ var cell = {
     isMarked: false
 }
 
-const MINE = '💣'
+const MINE = '💥'
 
 
 
@@ -30,6 +31,7 @@ const MINE = '💣'
 function onInInt() {
     gBoard = buildBoard(gLevel.SIZE, gLevel.SIZE)
     renderBoard(gBoard)
+    renderUI()
 
 }
 
@@ -50,12 +52,10 @@ function buildBoard(ROWS, COLS) {
             // if (i === 3 & j === 3) {
             //     board[i][j].isMine = true
             // }
-
         }
     }
     randomMines(board)
     setMinesNeighsCount(board)
-    console.log(board);
     return board
 
 }
@@ -69,13 +69,14 @@ function renderBoard(board) {
         for (var j = 0; j < board[0].length; j++) {
 
             const currCell = board[i][j]
-            const bombsAround = currCell.minesAroundCount
+            const minesAroundTile = currCell.minesAroundCount
+            const isMine = currCell.isMine
 
-            strHTML += `\t<td id=${bombsAround} data-i=${i} data-j=${j} class="cell hide" onclick="onCellClicked(this,${i},${j})" >\n`
+            strHTML += `\t<td id=${minesAroundTile} data-i=${i} data-j=${j} data-isMine=${isMine} class="cell hide" onclick="onCellClicked(this,${i},${j})" >\n`
             strHTML += '<span>'
 
-            if (currCell.isMine === true) strHTML += MINE
-            if (currCell.isMine === false) strHTML += bombsAround
+            if (currCell.isMine) strHTML += MINE
+            if (!currCell.isMine && minesAroundTile > 0) strHTML += minesAroundTile
             strHTML += '</span>'
 
             strHTML += '\t</td>\n'
@@ -92,8 +93,6 @@ function setMinesNeighsCount(board) {
         for (var j = 0; j < board.length; j++) {
             board[i][j].minesAroundCount = countNeighbors(i, j, board)
         }
-
-
     }
 
     return board
@@ -115,24 +114,21 @@ function countNeighbors(cellI, cellJ, board,) {
 
 function onCellClicked(cell, i, j) {
     if (gGame.isOn === false) return
-    gBoard[i][j].isShown = true
     const elCell = cell
+    const isMine = elCell.getAttribute('data-isMine')
+    if (elCell.classList.contains('shown')) return
     elCell.classList.add('shown')
-    const elCellId = elCell.getAttribute('id')
-
-    console.log(elCellId);
-    // if (elCellId === '0') {
-    //     revealNeighbors(i, j, gBoard,)
-    // }
-
+    if (isMine === 'true') gGame.lives--
     elCell.classList.remove('hide')
+    const elCellId = cell.getAttribute('id')
+    if (elCellId === '0') revealNeighbors(i, j, gBoard)
 
-
-
-
+    gBoard[i][j].isShown = true
+    gGame.shownCount++
+    renderUI()
+    gameState()
 
 }
-
 
 function randomMines(gBoard) {
     for (var i = 0; i <= gLevel.MINES - 1; i++) {
@@ -145,14 +141,31 @@ function randomMines(gBoard) {
     }
 }
 
+function getRandomInt(min, max) {
+    min = Math.ceil(min)
+    max = Math.floor(max)
+    return Math.floor(Math.random() * (max - min)) + min //The maximum is inclusive and the minimum is inclusive
+}
+
+function renderUI() {
+    const elLives = document.querySelector('.uI .lives')
+    elLives.innerHTML = gGame.lives
+    const elSmile = document.querySelector('.smily span')
+    if (gGame.lives === 0) elSmile.innerHTML = '🤯'
+}
+
+function gameState() {
+    if (gGame.lives === 0) gGame.isOn = false
 
 
-
-
-
-
-
-
+}
+function resetGame() {
+    gGame.lives = 3
+    gGame.isOn = true
+    const elSmile = document.querySelector('.smily span')
+    elSmile.innerHTML = '😊'
+    onInInt()
+}
 
 
 
@@ -169,35 +182,23 @@ function revealNeighbors(cellI, cellJ, board,) {
             if (currCell.isMine === false) {
                 const elCell = document.querySelector(`[data-i="${i}"][data-j="${j}"]`)
                 currCell.isShown = true
-                console.log(currCell.isShown);
+                gGame.shownCount++
                 elCell.classList.remove('hide')
-                console.log('updated', gBoard);
+                elCell.classList.add('shown')
             }
-            if (currCell.minesAroundCount === 0) revealNeighbors(i, j, gBoard,)
-
-
+            // if (currCell.minesAroundCount === 0) revealNeighbors(i, j, gBoard,)
         }
     }
 
 }
 
-
-function getRandomInt(min, max) {
-    min = Math.ceil(min)
-    max = Math.floor(max)
-    return Math.floor(Math.random() * (max - min)) + min //The maximum is inclusive and the minimum is inclusive
+function changeDifficulty(event) {
+    console.log(event);
+    if (event === 1) { gLevel.SIZE = 5 ,gLevel.MINES =2}
+    if (event === 2) { gLevel.SIZE = 8 ,gLevel.MINES =14}
+    if (event === 3) { gLevel.SIZE = 12 ,gLevel.MINES =32}
+    resetGame()
 }
-
-
-
-
-
-
-
-
-
-
-
 
 
 
